aad62be4998219cf3b2cad4aff71b2449ce51f19
“elphreu” <eplhreu@gmail.com>
2022-07-18 21:49:52 -0500

    Bootstrap !!!!

---
+++ js.js
@@ -0,0 +1,13 @@
+let num = Math.floor(Math.random() * 100);
+
+// alert(num);
+// alert(num);
+// if (num > 60) {
+
+// }
+document.getElementById("p-3").style.color = "red";
+// document.querySelector(".num").innerHTML = num;
+// let el = document.getElementById("num");
+
+// el.style.backgroundColor = red;
+
